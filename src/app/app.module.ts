67a1f099dafe1e6c0a16d558e052c6148faa633f
Ilya <kexit353@gmail.com>
2022-01-14 12:12:18 +0300

    add app-comp and module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,28 @@
-import { HttpClientModule } from '@angular/common/http';
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { HttpClientModule } from '@angular/common/http';
+import { MatButtonModule } from '@angular/material/button';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatIconModule } from '@angular/material/icon';
 
 import AppRoutingModule from './app-routing.module';
-import AppComponent from './app.component';
+import { AppComponent } from './app.component';
+import { HomeModule } from './components/home/home.module';
+import { NotFoundModule } from './components/not-found/not-found.module';
 
 @NgModule({
   declarations: [AppComponent],
-  imports: [BrowserModule, AppRoutingModule, HttpClientModule],
-  providers: [],
+  imports: [
+    MatButtonModule,
+    MatToolbarModule,
+    MatIconModule,
+    BrowserModule,
+    AppRoutingModule,
+    HttpClientModule,
+    HomeModule,
+    NotFoundModule,
+  ],
+
   bootstrap: [AppComponent],
 })
-export default class AppModule {}
+export class AppModule {}
